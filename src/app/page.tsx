f09395494ca80e3690ffd3f27ba75f648d93cc7c
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-04 09:06:49 -0300

    add home header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import HomeHeader from './components/HomeHeader'
+import Image from 'next/image'
+import HomeSearch from './components/HomeSearch'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,6 +9,17 @@ export default function Home() {
   return (
     <main className="">
       <HomeHeader />
+
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          alt='Google logo'
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+        />
+
+        <HomeSearch />
+      </div>
     </main>
   )
 }
