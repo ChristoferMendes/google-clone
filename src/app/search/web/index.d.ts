3b32e1e1bd8eebd1a435126f663a91ae193e4a73
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-11 09:03:01 -0300

    fetching data from google api and showing results

---
+++ src/app/search/web/index.d.ts
@@ -0,0 +1,163 @@
+export interface Pokedex {
+  data: Data;
+}
+
+export interface Data {
+  kind:              string;
+  url:               URL;
+  queries:           Queries;
+  context:           Context;
+  searchInformation: SearchInformation;
+  items:             Item[];
+}
+
+export interface Context {
+  title: string;
+}
+
+export interface Item {
+  kind:             Kind;
+  title:            string;
+  htmlTitle:        string;
+  link:             string;
+  displayLink:      string;
+  snippet:          string;
+  htmlSnippet:      string;
+  cacheId?:         string;
+  formattedUrl:     string;
+  htmlFormattedUrl: string;
+  pagemap:          Pagemap;
+}
+
+export enum Kind {
+  CustomsearchResult = "customsearch#result",
+}
+
+export interface Pagemap {
+  metatags:               Metatag[];
+  hcard?:                 Hcard[];
+  cse_thumbnail?:         CSEThumbnail[];
+  imageobject?:           Imageobject[];
+  person?:                Person[];
+  cse_image?:             CSEImage[];
+  broadcastevent?:        Broadcastevent[];
+  webpage?:               Sitenavigationelement[];
+  sitenavigationelement?: Sitenavigationelement[];
+}
+
+export interface Broadcastevent {
+  duration?: string;
+  name:      string;
+  startdate: string;
+  url:       string;
+}
+
+export interface CSEImage {
+  src: string;
+}
+
+export interface CSEThumbnail {
+  src:    string;
+  width:  string;
+  height: string;
+}
+
+export interface Hcard {
+  fn:  string;
+  url: string;
+}
+
+export interface Imageobject {
+  width:  string;
+  url:    string;
+  height: string;
+}
+
+export interface Metatag {
+  referrer?:                      string;
+  "og:image"?:                    string;
+  "theme-color"?:                 string;
+  "og:image:width"?:              string;
+  "og:type"?:                     string;
+  viewport?:                      string;
+  "og:title"?:                    string;
+  "og:image:height"?:             string;
+  "format-detection"?:            string;
+  "twitter:app:url:iphone"?:      string;
+  "twitter:app:id:googleplay"?:   string;
+  "twitter:card"?:                string;
+  "og:site_name"?:                string;
+  "twitter:url"?:                 string;
+  "twitter:app:url:ipad"?:        string;
+  "al:android:package"?:          string;
+  "twitter:app:name:googleplay"?: string;
+  "al:ios:url"?:                  string;
+  "twitter:app:id:iphone"?:       string;
+  "og:description"?:              string;
+  "al:ios:app_store_id"?:         string;
+  "twitter:image"?:               string;
+  "twitter:site"?:                string;
+  "twitter:title"?:               string;
+  "al:ios:app_name"?:             string;
+  "twitter:app:id:ipad"?:         string;
+  "al:web:url"?:                  string;
+  "og:video:tag"?:                string;
+  "al:android:url"?:              string;
+  "fb:app_id"?:                   string;
+  "twitter:app:url:googleplay"?:  string;
+  "twitter:app:name:ipad"?:       string;
+  "twitter:description"?:         string;
+  "og:url"?:                      string;
+  "al:android:app_name"?:         string;
+  "twitter:app:name:iphone"?:     string;
+  "msapplication-tilecolor"?:     string;
+  "twitter:aria-text"?:           string;
+  "og:aria-text"?:                string;
+  author?:                        string;
+  "msapplication-tileimage"?:     string;
+  image?:                         string;
+  "twitter:creator"?:             string;
+  "article:published_time"?:      Date;
+  "article:author"?:              string;
+  created?:                       string;
+  title?:                         string;
+  revised?:                       string;
+}
+
+export interface Person {
+  name: string;
+  url:  string;
+}
+
+export interface Sitenavigationelement {
+  headline: string;
+}
+
+export interface Queries {
+  request:  NextPage[];
+  nextPage: NextPage[];
+}
+
+export interface NextPage {
+  title:          string;
+  totalResults:   string;
+  searchTerms:    string;
+  count:          number;
+  startIndex:     number;
+  inputEncoding:  string;
+  outputEncoding: string;
+  safe:           string;
+  cx:             string;
+}
+
+export interface SearchInformation {
+  searchTime:            number;
+  formattedSearchTime:   string;
+  totalResults:          string;
+  formattedTotalResults: string;
+}
+
+export interface URL {
+  type:     string;
+  template: string;
+}
--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,30 @@
-export default function WebSearchPage() {
+import { Data } from "."
+
+const getSearchData = async (search: string | undefined) => {
+  if (!search) return
+
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${search}`)
+  const data: Data | undefined = await response.json()
+
+  return data;
+
+}
+
+export default async function WebSearchPage({ searchParams }: { searchParams: { searchTerm: string } | undefined } ) {
+  if (!searchParams) {
+    throw new Error()
+  }
+  
+  const searchData = await getSearchData(searchParams?.searchTerm)
+
+
   return (
-    <div>WebSearchPage</div>
+    <>
+      {searchData && searchData.items.map(item => (
+        <h1 key={item.cacheId}>{item.title}</h1>
+      ))}
+    </>
   )
+
+
 }
