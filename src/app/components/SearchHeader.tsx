e48c2eaff410cfb7e36216591eab440266e7ec67
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-12 18:02:29 -0300

    Add hamburger menu and hide sign in button on small screens

--- src/app/components/SearchHeader.tsx
+++ src/app/components/SearchHeader.tsx
@@ -4,13 +4,16 @@ import SearchBox from "./SearchBox";
 import { RiSettings3Line } from 'react-icons/ri'
 import { TbGridDots } from 'react-icons/tb'
 import SearchHeaderOptions from "./SearchHeaderOptions";
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 export default function SearchHeader() {
   return (
     <header className="sticky top-0 bg-white">
       <div className="flex w-full p-6 items-center justify-between">
         <Link href={'/'}>
-          <Image alt="google logo" width={'120'} height={'40'} src={'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png'} />
+          <Image alt="google logo" width={'120'} height={'40'} src={'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png'} 
+          className="hidden sm:flex"
+          />
         </Link>
         <div className="flex-1">
           <SearchBox />
@@ -19,7 +22,10 @@ export default function SearchHeader() {
           <RiSettings3Line className="header-icon" />
           <TbGridDots className="header-icon" />
         </div>
-        <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2'>Sign in</button>
+        <button className='hidden xsm:flex bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2'>Sign in</button>
+        <div>
+          <GiHamburgerMenu className="text-xl xsm:hidden"/>
+        </div>
       </div>
       <SearchHeaderOptions />
     </header>
--- src/app/components/WebSearchResults.tsx
+++ src/app/components/WebSearchResults.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { Data } from "../search";
 import Parser from "html-react-parser";
 
 export default function WebSearchResults({ results }: { results: Data }) {
+  console.log(results.items)
+
+
   return (
     <div className="w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[13%] lg:pl-48">
       <p className="text-gray-600 text-sm mb-5 mt-3">
@@ -11,11 +15,17 @@ export default function WebSearchResults({ results }: { results: Data }) {
       {results.items.map(result => (
         <div className="mb-8 max-w-xl " key={result.cacheId}>
           <div className="group flex flex-col">
-            <Link className="text-sm truncate" href={result.link}>
-              {result.formattedUrl}
+            <Link href={result.link} className="flex">
+              <img src={result.pagemap.cse_image?.[0].src} alt="" className="w-12 rounded-full h-12" />
+              <div>
+                <p className="text-md">{result.displayLink}</p>
+                <p className="text-sm truncate text-zinc-700">{result.formattedUrl}</p>
+              </div>
+
+
             </Link>
             <Link href={result.link}>
-              <h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline decoration-blue-800">{result.title}</h2>
+              <h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline decoration-blue-800">{Parser(result.htmlTitle)}</h2>
             </Link>
           </div>
           <p className="text-gray-700">{Parser(result.htmlSnippet)}</p>
